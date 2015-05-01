96cee2182447f9685f8eb9a76a221d121b29cc84
laixintao <laixintao1995@163.com>
2015-05-02 02:47:30 +0800

    开始写牌型判断
    内部的七张牌应该使用数组表示，方便操作。

--- process.js
+++ process.js
@@ -40,23 +40,94 @@ $(function(){
 		i=0;
 		$("option:selected :even").each(function(){
 			var color = $(this).text();
-			if(color ==='♥') color = 'b' ;
-			if(color ==='♠') color = 'a' ;
-			if(color ==='♣') color = 'c' ;
-			if(color ==='♦') color = 'd' ;
+			if(color ==='♥') color = '0' ;
+			if(color ==='♠') color = '1' ;
+			if(color ==='♣') color = '2' ;
+			if(color ==='♦') color = '3' ;
 			colors[i] = color;
 			i++;
 		});
-		var RoyalStraightFlush;
-		var StraightFlush;
-		var Four;
-		var FullHouse;
-		var Flush;
-		var Straight;
-		var House;
-		var TwoPair;
-		var Pair;
-		var high;
+		var RoyalStraightFlush; //1
+		var StraightFlush;		//2
+		var Four;				//3
+		var FullHouse;			//4
+		var Flush;				//5
+		var Straight;			//6
+		var House;				//7
+		var TwoPair;			//8
+		var Pair;				//9
+		var high;				//10
+
+
 	});
 
 });
+
+
+function compare(a,b){
+
+
+}
+
+function creat_player(ahand1,ahand2){
+	var player = new Object;
+	player.hand1 = ahand1;
+	player.hand2 = ahand2;
+	player.flop1 = null;
+	player.flop2 = null;
+	player.flop3 = null;
+	player.turn = null;
+	player.river = null;
+
+	player.type = null;
+
+	player.Flush = null;
+	player.Four = null;
+
+	function is_Flush(){
+		var color_num[5];
+		color_num[player.hand1.color]++;
+		color_num[player.hand2.color]++;
+		color_num[player.flop1.color]++;
+		color_num[player.flop2.color]++;
+		color_num[player.flop3.color]++;
+		color_num[player.turn.color]++;
+		color_num[player.river.color]++;
+		for(i in color_num){
+			if(i>=5) player.Flush = true;
+		}
+		return player.Flush;
+	}
+
+	function not_Flush_Straight(){
+		var value_num[14];
+		var TYPE[5];
+		value_num[player.hand1.value]++;
+		value_num[player.hand2.value]++;
+		value_num[player.flop1.value]++;
+		value_num[player.flop2.value]++;
+		value_num[player.flop3.value]++;
+		value_num[player.turn.value]++;
+		value_num[player.river.value]++;
+		for(i in value_num){
+			TYPE[value_num[i]]++;
+		}
+		if(TYPE[4]!=null){
+			player.type = 3;
+			///////////////////////////////////////////////////continue
+		}
+		return player.Flush;
+	}
+
+	function get_type(){
+		//RoyalStraightFlush
+	}
+	return player; 
+}
+
+function new_card(acolor,avalue){
+	var card = new Object;
+	card.color = acolor;
+	card.value = avalue;
+	return card;
+}
